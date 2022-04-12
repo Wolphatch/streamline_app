d2ce82745c8950c44989de89bf9c78c2c0713536
Wolphatch <31427525+Wolphatch@users.noreply.github.com>
2022-04-12 15:46:05 +1000

    Update streamlit_app.py
    emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("-------------Dish 1-------------")
-streamlit.text("-------------Dish 2-------------")
-streamlit.text("-------------Dish 3-------------")
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
