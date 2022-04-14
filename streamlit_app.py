69b38932958580f42adf87a2b7c805743191ffcc
Wolphatch <31427525+Wolphatch@users.noreply.github.com>
2022-04-14 11:27:27 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
+import snowflake.connector
 import streamlit
+
 
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
